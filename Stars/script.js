9aaa709726232717a4817a5b0f542b17f07943c0
mohammad ilia <mmdiliataha73@gmail.com>
2025-10-09 21:22:55 +0330

    debugging the stars to the fastest way

--- Stars/script.js
+++ Stars/script.js
@@ -1,8 +1,8 @@
 let Input = Number(prompt('Enter your number: '))
 let star = '*';
-for(let i=1;i<Input+1;i++){
+for(let i=1;i<=Input;i++){
     let result = "";
-    for(let I=1;I<i+1;I++){
+    for(let I=1;I<=i;I++){
         result += star;
     }
     console.log(result);
